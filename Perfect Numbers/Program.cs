07c4442e0a5f7d6fa239515e7a8b7678e87ed354
yousef55555 <121629897+yousef55555@users.noreply.github.com>
2022-12-30 01:09:07 +0200

    Add files via upload

---
+++ Perfect Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // raed the input numbers start number and end number
+
+            Console.WriteLine("Enter The Start Number: ");
+            int startnumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter The End Number: ");
+            int endnumber = int.Parse(Console.ReadLine());
+
+            // find the perfect numbers between start number and end number 
+            for (int i = startnumber; i <= endnumber; i++)
+            {
+                // check if i is a perfect number
+
+                int sum = 0;
+
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
